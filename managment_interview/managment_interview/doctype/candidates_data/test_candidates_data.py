e6e474112ece319b96b8cf55a5379cb0ea33a06d
sapnabhamdare123 <sapnabhamdare@gmail.com>
2024-09-18 16:43:11 +0530

    this is new

---
+++ managment_interview/managment_interview/doctype/candidates_data/test_candidates_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, sapna and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCandidatesData(FrappeTestCase):
+	pass
